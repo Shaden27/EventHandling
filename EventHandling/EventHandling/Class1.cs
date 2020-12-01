8a2b509ba1ac915d363e4b782a198046d03f0607
Shreepad Divekar <54166955+Shaden27@users.noreply.github.com>
2020-12-01 14:13:56 +0530

    Add files via upload

---
+++ EventHandling/EventHandling/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandling
+{
+    class Bank
+    {
+        public double netBal;
+        public Bank()
+        {
+            netBal = 5000;
+        }
+        public Bank(double b)
+        {
+            netBal = b;
+        }
+        public void deposit(double dep)
+        {
+            double test = netBal + dep;
+            if (netBal > 100000)
+            {
+                netBal = netBal + 0.7 * dep;
+                System.Windows.Forms.MessageBox.Show("Tax Cut");
+            }
+            else if (test > 100000)
+            {
+                test = test - 100000;
+                test = 0.3 * test;
+                netBal = netBal + dep - test;
+                System.Windows.Forms.MessageBox.Show("Tax Cut");
+            }
+            else
+            {
+                netBal = netBal + dep;
+            }
+        }
+        public void withdraw(double wit)
+        {
+            double test = netBal - wit;
+            if (test < 5000)
+            {
+                System.Windows.Forms.MessageBox.Show("Cannot Withdraw Minimun Balance should ve 5000");
+            }
+            else
+            {
+                netBal = netBal - wit;
+            }
+        }
+    }
+}
---
+++ EventHandling/EventHandling/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventHandling
+{
+    public partial class Form1 : Form
+    {
+        Bank bank;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dep = Convert.ToDouble(textBox1.Text);
+            bank.deposit(dep);
+            label1.Text = Convert.ToString(bank.netBal);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bank = new Bank();
+            label1.Text = Convert.ToString(bank.netBal);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double wit = Convert.ToDouble(textBox2.Text);
+            bank.withdraw(wit);
+            label1.Text = Convert.ToString(bank.netBal);
+        }
+    }
+}
